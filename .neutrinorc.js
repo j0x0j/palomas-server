8885e1a9491df21147ebb0033657afe8e440c990
Joel Martinez <jmgalanes@gmail.com>
2017-10-09 13:46:51 -0400

    Prepering frontend build process for deployment

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,6 +1,11 @@
 module.exports = {
   use: [
     'neutrino-preset-standard',
-    'neutrino-preset-react'
+    'neutrino-preset-react',
+    ['neutrino-middleware-html-template', {
+      title: 'Palomas Mensajeras',
+      mobile: true
+    }],
+    'neutrino-middleware-minify'
   ]
 };
