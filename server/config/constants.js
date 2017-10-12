ef75c3b950e670a84e29756f24d8f18647fb460e
Joel Martinez <jmgalanes@gmail.com>
2017-10-12 14:24:09 -0400

    Use env for twilio from number

--- server/config/constants.js
+++ server/config/constants.js
@@ -3,5 +3,5 @@ const env = process.env.NODE_ENV
 module.exports = {
   TWILIO_SID: (env === 'testing' ? process.env.TWILIO_SID_TEST : process.env.TWILIO_SID),
   TWILIO_TOK: (env === 'testing' ? process.env.TWILIO_TOK_TEST : process.env.TWILIO_TOK),
-  TWILIO_NUM: (env === 'testing' ? process.env.TWILIO_TEST_FROM_NUMBER : '17873053701'),
+  TWILIO_NUM: (env === 'testing' ? process.env.TWILIO_TEST_FROM_NUMBER : process.env.TWILIO_FROM_NUMBER),
 }
